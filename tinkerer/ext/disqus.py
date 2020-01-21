04f1ef0611f2066c89acd9bbffea1bfcd75e31be
Tommy109 <tokutomi@degino.com>
2020-01-21 15:06:26 +0900

    fix: change disqus protocol to https.

--- tinkerer/ext/disqus.py
+++ tinkerer/ext/disqus.py
@@ -30,7 +30,7 @@ def create_thread(disqus_shortname, identifier):
 '    disqus_thread();'
 '</script>'
 '<noscript>Please enable JavaScript to view the '
-'   <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a>'
+'   <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a>'
 '</noscript>' % (disqus_shortname, identifier))
 
 
